b7869d36fb8d8153066fa6a33503addee7ebca3a
jrwill217 <williamshenjr@gmail.com>
2024-01-15 22:37:19 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+let x = 1;
+x = 6;
+x = 5;
+
+const y = 3;
+console.log(y);
+
+let arr = [6,7,2,4,0,6,"string"];
+
+
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i])
+    console.log("array"+[i]+":"+arr[0])
+}
+
+if(x == 6){
+    console.log("yes")
+}else{
+    console.log("no")
+}
+
+for(let i = 0; i < 5; i++){
+}
+
+let hi = document.getElementById("id1")
+console.log(hi)
+
+//this function is doing blabla
+function changeColor(color1){
+    hi.style.color = color1
+    return color1
+}
+
+function myfunction(p1,p2){
+    return "hello"+1;
+}
+
+console.log(changeColor("red"))
